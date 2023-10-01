3fbbeae65572b29670f29780f20e71252163f262
nour edden <noureddenrz@outlook.com>
2023-10-01 22:04:14 +0800

    added the second button and changed the second canvas

--- canvas.js
+++ canvas.js
@@ -1,10 +1,14 @@
-function changeColor(){
+function changeColorBlue(){
     var divElement1 =
     document.getElementById("div1");
+    
+    divElement1.className= "blueback";
+}
+function changeColorRed(){
+
     var divElement2 =
     document.getElementById("div2");
-    divElement1.className= "blueback";
-    divElement2.className= "fushiaback";
+    divElement2.className= "Orangeback";
 }
 //const myCanvas = document.getElementById("myCanvas");
 //const ctx = myCanvas.getContext("2d");
