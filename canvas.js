120ad805073381f20fe26c40169f07d1ab95145f
nour edden <noureddenrz@outlook.com>
2023-10-01 22:26:36 +0800

    used the context property

--- canvas.js
+++ canvas.js
@@ -6,9 +6,19 @@ function changeColorBlue(){
 }
 function changeColorRed(){
 
-    var divElement2 =
-    document.getElementById("div2");
-    divElement2.style.backgroundColor= "red";
+    var canvas = document.getElementById("div2");
+    canvas.style.backgroundColor = "red";
+    var context  = canvas.getContext("2d");
+    context.fillStyle = "yellow";
+    context.fillRect(10,10,60,60);
+    context.fillRect(80,10,80,60);
+
+    context.fillStyle = "black";
+    context.font = "20px Arial";
+    context.fillText("Hello", 15, 50);
+    context.fillStyle = "black";
+    context.font = "20px Arial";
+    context.fillText("World", 85, 50);
 }
 //const myCanvas = document.getElementById("myCanvas");
 //const ctx = myCanvas.getContext("2d");
