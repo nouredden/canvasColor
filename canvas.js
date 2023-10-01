ee10476f6450ce1cab1935345e2bd3092cc0bdc9
nour edden <noureddenrz@outlook.com>
2023-10-01 21:55:05 +0800

    changed the language

--- canvas.js
+++ canvas.js
@@ -1,5 +1,11 @@
 function changeColor(){
-const myCanvas = document.getElementById("myCanvas");
-const ctx = myCanvas.getContext("2d");
-ctx.fillRect(20, 20, 150, 100);
+    var divElement1 =
+    document.getElementById("div1");
+    var divElement2 =
+    document.getElementById("div2");
+    divElement1.className= "blueback";
+    divElement2.className= "fushiaback";
 }
+//const myCanvas = document.getElementById("myCanvas");
+//const ctx = myCanvas.getContext("2d");
+//ctx.fillRect(20, 20, 150, 100);
